a65663caba9887a5bba2c4a4cad3b5ce1108cce2
Marco Ruffolo <marcoruffolo2003@gmail.com>
2026-04-01 17:53:55 -0300

    ProductManager

---
+++ src/managers/ProductManager.js
@@ -0,0 +1,63 @@
+import fs from "fs/promises";
+
+class ProductManager {
+
+    constructor(path) {
+        this.path = path;
+    }
+
+    async getProducts() {
+        let data = await fs.readFile(this.path, "utf-8");
+        let result = JSON.parse(data);
+        return result;
+    }
+
+    async getProductById(id) {
+        const products = await this.getProducts();
+        const product = products.find(product => product.id === id);
+        if (!product) {
+            return null;
+        }
+        return product;
+    }
+
+    async addProduct(productData) {
+
+        const products = await this.getProducts();
+        const newProduct = {
+            id: Date.now(),
+            ...productData
+        }
+        products.push(newProduct);
+        await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+        return newProduct;
+    }
+
+    async updateProduct(id, updatedData) {
+        const products = await this.getProducts();
+        const index = products.findIndex((p) => p.id === id);
+        if (index === -1) {
+            return null;
+        }
+        const { id: _, ...dataWithoutId } = updatedData;
+        products[index] = {
+            ...products[index],
+            ...dataWithoutId
+        }
+        await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+        return products[index];
+    }
+
+    async deleteProduct(id) {
+        const products = await this.getProducts();
+        const index = products.findIndex((p) => p.id === id);
+        if (index === -1) {
+            return null;
+        }
+        const deletedProduct = products.splice(index, 1);
+        await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+        return deletedProduct[0];
+    }
+}
+
+export default ProductManager;
