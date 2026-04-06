72f94afd1633219b95d4e77cae42834e60ceb02f
Marco Ruffolo <marcoruffolo2003@gmail.com>
2026-04-06 19:10:04 -0300

    cart manager con mongoose

--- src/managers/cartManager.js
+++ src/managers/cartManager.js
@@ -1,42 +1,29 @@
-import fs from "fs/promises";
+import Cart from "../models/Cart.js";
 
-class CartManager {
-    constructor(path) {
-        this.path = path;
-    }
+class CartManager {   
 
     async getCarts() {
-        const data = await fs.readFile(this.path, "utf-8");
-        return JSON.parse(data);
+        return await Cart.find();
     }
 
     async createCart() {
-        const carts = await this.getCarts();
-        const newCart = {
-            id: Date.now(),
-            products: []
-        }
-        carts.push(newCart);
-        await fs.writeFile(this.path, JSON.stringify(carts, null, 2));
-        return newCart;
+        return await Cart.create({ products: []});
     }
 
-    async addProductToCart(cartId, productId) {
-        const carts = await this.getCarts();
-        const index = carts.findIndex((c) => c.id === cartId);
-        if (index === -1) {
-            return null;
-        }
-        const productIndex = carts[index].products.findIndex((p) => p.product === productId);
+    async addProductToCart(cartId, productId) {       
+        const cart = await Cart.findById(cartId);
+        if(!cart) return null; 
+        const productIndex = cart.products.findIndex((p) => p.product.toString() === productId.toString());
 
-        if (productIndex != -1) {
-            carts[index].products[productIndex].quantity += 1;
+        if(productIndex === -1){
+            cart.products.push({ product : productId, quantity : 1});
         }
-        else {
-            carts[index].products.push({ product: productId, quantity: 1 });
+        else{
+            cart.products[productIndex].quantity += 1;
         }
-        await fs.writeFile(this.path, JSON.stringify(carts, null, 2));
-        return carts[index];
+        
+        await cart.save();
+        return cart;
     }
 }
 
